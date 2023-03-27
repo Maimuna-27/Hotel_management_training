7ba15014694cbb5af287edc4ec2cee17713cb9cb
Mainmuna-27 <mymunaabdulrahman@gmail.com>
2023-03-27 12:47:34 +0530

    test

--- customapp/customapp/doctype/room_name/test_room_name.py
+++ customapp/customapp/doctype/room_name/test_room_name.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2023, mymuna and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestRoomName(FrappeTestCase):
 	pass
+
+	def test_our_roomname_document(self):
+		doc = frappe.get_doc({
+               "doctype" : "Room Name",
+			   "room_name" : "Deluxe Bedroom",
+			   "price" : "5000",
+			   
+			   
+		})
